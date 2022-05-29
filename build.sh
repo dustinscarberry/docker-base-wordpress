11fa314c3df1362fea2184c19dfa335137a3bf6f
Dustin Scarberry <bitsnbytes1001@gmail.com>
2022-05-28 23:43:54 -0400

    Optimize build

--- build.sh
+++ build.sh
@@ -7,15 +7,15 @@ docker login
 # build and push base docker images
 
 # vanilla wordpress
-docker build -t dustinscarberry/wordpress-base:php8.1 --build-arg PHP_VERSION=$php81Version -f 8.1/vanilla/Dockerfile .
+docker build -t dustinscarberry/wordpress-base:php8.1 --build-arg PHP_VERSION=$php81Version -f images/vanilla/Dockerfile .
 docker push dustinscarberry/wordpress-base:php8.1
 
-docker build -t dustinscarberry/wordpress-base:php8.1-cached --build-arg PHP_VERSION=$php81Version -f 8.1/vanilla-cached/Dockerfile .
+docker build -t dustinscarberry/wordpress-base:php8.1-cached --build-arg PHP_VERSION=$php81Version -f images/vanilla-cached/Dockerfile .
 docker push dustinscarberry/wordpress-base:php8.1-cached
 
 # bedrock
-docker build -t dustinscarberry/wordpress-base:php8.1-bedrock --build-arg PHP_VERSION=$php81Version -f 8.1/bedrock/Dockerfile .
+docker build -t dustinscarberry/wordpress-base:php8.1-bedrock --build-arg PHP_VERSION=$php81Version -f images/bedrock/Dockerfile .
 docker push dustinscarberry/wordpress-base:php8.1-bedrock
 
-docker build -t dustinscarberry/wordpress-base:php8.1-bedrock-cached --build-arg PHP_VERSION=$php81Version -f 8.1/bedrock-cached/Dockerfile .
+docker build -t dustinscarberry/wordpress-base:php8.1-bedrock-cached --build-arg PHP_VERSION=$php81Version -f images/bedrock-cached/Dockerfile .
 docker push dustinscarberry/wordpress-base:php8.1-bedrock-cached

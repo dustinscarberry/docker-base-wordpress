e8da25b6881746005a938165b8e730d32d210d57
Dustin Scarberry <bitsnbytes1001@gmail.com>
2023-06-06 13:47:29 -0400

    PHP updates / image rename

--- build.sh
+++ build.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 
-php81Version=php:8.1.18-fpm-alpine
-php82Version=php:8.2.5-fpm-alpine
+php81Version=php:8.1.19-fpm-alpine
+php82Version=php:8.2.6-fpm-alpine
 
 docker login
 
 # build and push base docker images
 
 # vanilla wordpress
-docker build -t dustinscarberry/wordpress-base:php8.1 --build-arg PHP_VERSION=$php81Version -f images/vanilla/Dockerfile .
-docker push dustinscarberry/wordpress-base:php8.1
+docker build -t dustinscarberry/wordpress:php8.1 --build-arg PHP_VERSION=$php81Version -f images/vanilla/Dockerfile .
+docker push dustinscarberry/wordpress:php8.1
 
-docker build -t dustinscarberry/wordpress-base:php8.1-cached --build-arg PHP_VERSION=$php81Version -f images/vanilla-cached/Dockerfile .
-docker push dustinscarberry/wordpress-base:php8.1-cached
+docker build -t dustinscarberry/wordpress:php8.1-cached --build-arg PHP_VERSION=$php81Version -f images/vanilla-cached/Dockerfile .
+docker push dustinscarberry/wordpress:php8.1-cached
 
-docker build -t dustinscarberry/wordpress-base:php8.2 --build-arg PHP_VERSION=$php82Version -f images/vanilla/Dockerfile .
-docker push dustinscarberry/wordpress-base:php8.2
+docker build -t dustinscarberry/wordpress:php8.2 --build-arg PHP_VERSION=$php82Version -f images/vanilla/Dockerfile .
+docker push dustinscarberry/wordpress:php8.2
 
-docker build -t dustinscarberry/wordpress-base:php8.2-cached --build-arg PHP_VERSION=$php82Version -f images/vanilla-cached/Dockerfile .
-docker push dustinscarberry/wordpress-base:php8.2-cached
+docker build -t dustinscarberry/wordpress:php8.2-cached --build-arg PHP_VERSION=$php82Version -f images/vanilla-cached/Dockerfile .
+docker push dustinscarberry/wordpress:php8.2-cached
 
 # bedrock
-docker build -t dustinscarberry/wordpress-base:php8.1-bedrock --build-arg PHP_VERSION=$php81Version -f images/bedrock/Dockerfile .
-docker push dustinscarberry/wordpress-base:php8.1-bedrock
+docker build -t dustinscarberry/wordpress:php8.1-bedrock --build-arg PHP_VERSION=$php81Version -f images/bedrock/Dockerfile .
+docker push dustinscarberry/wordpress:php8.1-bedrock
 
-docker build -t dustinscarberry/wordpress-base:php8.1-bedrock-cached --build-arg PHP_VERSION=$php81Version -f images/bedrock-cached/Dockerfile .
-docker push dustinscarberry/wordpress-base:php8.1-bedrock-cached
+docker build -t dustinscarberry/wordpress:php8.1-bedrock-cached --build-arg PHP_VERSION=$php81Version -f images/bedrock-cached/Dockerfile .
+docker push dustinscarberry/wordpress:php8.1-bedrock-cached
 
-docker build -t dustinscarberry/wordpress-base:php8.2-bedrock --build-arg PHP_VERSION=$php82Version -f images/bedrock/Dockerfile .
-docker push dustinscarberry/wordpress-base:php8.2-bedrock
+docker build -t dustinscarberry/wordpress:php8.2-bedrock --build-arg PHP_VERSION=$php82Version -f images/bedrock/Dockerfile .
+docker push dustinscarberry/wordpress:php8.2-bedrock
 
-docker build -t dustinscarberry/wordpress-base:php8.2-bedrock-cached --build-arg PHP_VERSION=$php82Version -f images/bedrock-cached/Dockerfile .
-docker push dustinscarberry/wordpress-base:php8.2-bedrock-cached
+docker build -t dustinscarberry/wordpress:php8.2-bedrock-cached --build-arg PHP_VERSION=$php82Version -f images/bedrock-cached/Dockerfile .
+docker push dustinscarberry/wordpress:php8.2-bedrock-cached
